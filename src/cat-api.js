e3c6a2796e0614d38c572ada47228c05ec9cbc97
Andyoase <136448630+Andyoase@users.noreply.github.com>
2024-02-26 17:28:57 +0100

    update

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,19 +1,15 @@
-fetch("https://api.thecatapi.com/v1/images/search?size=small&breed_ids=abys", {
-  "headers": {
-    "accept": "*/*",
-    "accept-language": "ro-RO,ro;q=0.9",
-    "content-type": "application/json",
-    "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Opera\";v=\"106\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "cross-site",
-    "x-api-key": "live_aCUCbDYfzGa84sb0QsAvTm5exbHAyaYoR3hwd7LAfwqd6hyA2fhdEdcxvhbA6o7n"
-  },
-  "referrerPolicy": "same-origin",
-  "body": null,
-  "method": "GET",
-  "mode": "cors",
-  "credentials": "omit"
-});
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_aCUCbDYfzGa84sb0QsAvTm5exbHAyaYoR3hwd7LAfwqd6hyA2fhdEdcxvhbA6o7n";
+
+export const fetchBreeds = () => {
+    return axios.get("https://api.thecatapi.com/v1/breeds")
+        .then(response => response.data)
+        .catch(error => Promise.reject(error));
+};
+
+export const fetchCatByBreed = (breedId) => {
+    return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+        .then(response => response.data)
+        .catch(error => Promise.reject(error));
+};
